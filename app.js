9d3725a1164344cf4d87fd0147cf70f8f09d3902
Vishv <krishnakakadiya9@gmai.com>
2021-02-07 21:59:36 +0530

    adeed

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ const cors = require('cors');
 const memeRoutes = require("./routes/meme.js");
 const port = process.env.PORT || 8001;
 //DATABASE CONNECTION WITH MONGODB
+
+
+app.use(bodyParser.json())
+app.use(cookieparser());
+app.use(cors());
+app.use(express.static('dist'));
+//ROUTES
+app.get('/',(req,res,next)=>{
+    return json({
+        'message': "Done"
+    })
+})
+// app.use("/memes",memeRoutes);
+
 mangoose
     .connect(process.env.DB || "mongodb://localhost/testDB",{
         useNewUrlParser:true,
@@ -19,15 +33,6 @@ mangoose
         }).catch((err) =>{
             console.log(err);
 });
-
-app.use(bodyParser.json())
-app.use(cookieparser());
-app.use(cors());
-app.use(express.static('dist'));
-//ROUTES
-app.use("/memes",memeRoutes);
-
-
 app.listen(port, () => {
     console.log(`app is running on ${port}`);
 });
