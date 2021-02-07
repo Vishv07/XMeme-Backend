fd1d0004cb4990f69ffd4190bae390826dab86ba
Vishv <krishnakakadiya9@gmai.com>
2021-02-07 22:38:26 +0530

    routes enabled

--- routes/meme.js
+++ routes/meme.js
@@ -4,13 +4,12 @@ var router = express.Router();
 const {newMeme,getAllMemes,getMemeByID,updateMeme} = require("../controllers/memeController.js")
 
 // To Create a new Meme
-
 router.post('',[
     check("name","Plese enter a valid meme").isLength({min:3})
 ],newMeme);
 
 // To Fetch all memes
-router.get('/demo',getAllMemes);
+router.get('',getAllMemes);
 
 // To Fetch meme by ID
 router.get('/:id',getMemeByID);
